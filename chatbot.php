42212d4450bf77fce4b059972b310c4720d6944b
kanyamat <kanyamat003@gmail.com>
2017-11-07 12:00:53 +0700

    h

--- chatbot.php
+++ chatbot.php
@@ -136,10 +136,10 @@ if (!is_null($events['events'])) {
                  $replyToken = $event['replyToken'];
                  $messages = [
                         'type' => 'text',
-                        'text' =>  "aaaa"
+                        'text' =>  $answer
                       ];
 
-                $q = pg_exec($dbconn, "INSERT INTO users_register(user_id,user_name,user_surname,user_age,user_height,user_pre_weight,user_weight,preg_week,phone_number,hospital_name,hospital_number,history_medicine,history_food,status,updated_at )VALUES('{$user_id}','{$name}','{$surname}','','','','','','','','','','','1',NOW())") or die(pg_errormessage());
+                // $q = pg_exec($dbconn, "INSERT INTO users_register(user_id,user_name,user_surname,user_age,user_height,user_pre_weight,user_weight,preg_week,phone_number,hospital_name,hospital_number,history_medicine,history_food,status,updated_at )VALUES('{$user_id}','{$name}','{$surname}','','','','','','','','','','','1',NOW())") or die(pg_errormessage());
 
 
 
