708afdfdb9755faaa9d78b87e78da3671df10c0b
kanyamat <kanyamat003@gmail.com>
2017-11-07 11:57:46 +0700

    4

--- chatbot.php
+++ chatbot.php
@@ -139,7 +139,7 @@ if (!is_null($events['events'])) {
                         'text' =>  "aaaa"
                       ];
 
-                $q = pg_exec($dbconn, "INSERT INTO users_register(user_id,user_name,user_surname,user_age,user_height,user_pre_weight,user_weight,preg_week,phone_number,hospital_name,hospital_number,history_medicine,history_food,status,updated_at )VALUES('{$user_id}','{$name}','{$surname}','','','','','','','','','','','','')") or die(pg_errormessage());
+                $q = pg_exec($dbconn, "INSERT INTO users_register(user_id,user_name,user_surname,user_age,user_height,user_pre_weight,user_weight,preg_week,phone_number,hospital_name,hospital_number,history_medicine,history_food,status,updated_at )VALUES('{$user_id}','{$name}','{$surname}','','','','','','','','','','','1',NOW())") or die(pg_errormessage());
 
 
 
