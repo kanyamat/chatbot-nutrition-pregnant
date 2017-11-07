b979af928f5ecd2ee45617b7ff4c5631e9d77a96
C:\Users\Administrator\Desktop\pybotbot <kanyamat003@gmail.com>
2017-11-07 23:28:55 +0700

    f

--- chatbot.php
+++ chatbot.php
@@ -700,7 +700,7 @@ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextse
                   ]
                 ];
 
-}elseif ($event['message']['text'] == "แพ้ยา" && $seqcode == "0021"  ) {
+}elseif ($event['message']['text'] == "แพ้ยา" && $seqcode == "0022"  ) {
                $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
                 while ($row = pg_fetch_row($result)) {
                   echo $answer = $row[0]; /*ก่อนอื่น ดิฉันขออนุญาตถามข้อมูลเบื้องต้นเกี่ยวกับคุณก่อนนะคะ
@@ -721,7 +721,7 @@ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextse
 
 
 $q = pg_exec($dbconn, "UPDATE users_register SET history_medicine = $answer WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
-$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0021','','1001','0',NOW(),NOW())") or die(pg_errormessage());
+$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0022','','1001','0',NOW(),NOW())") or die(pg_errormessage());
 
 
 
