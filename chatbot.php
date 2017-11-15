408a47f8f72e722264084a503a3d0cea215f79b6
kanyamat <kanyamat003@gmail.com>
2017-11-15 13:17:00 +0700

    d

--- chatbot.php
+++ chatbot.php
@@ -901,62 +901,48 @@ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextse
 ########################################################################################################################################################
 
 
-// }elseif (strpos($_msg) !== false && $seqcode == "0027" ) {
-//                $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
-//                 while ($row = pg_fetch_row($result)) {
-//                   echo $answer = $row[0]; 
-//                 }   
-//             $u = pg_escape_string($_msg); 
-// $q = pg_exec($dbconn, "UPDATE users_register SET  history_food = '{$_msg}' WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
-//       $replyToken = $event['replyToken'];
+}elseif (strpos($_msg) !== false && $seqcode == "0027" ) {
+               $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
+                while ($row = pg_fetch_row($result)) {
+                  echo $answer = $row[0]; 
+                }   
+            $u = pg_escape_string($_msg); 
 
 
 
 
+        $messages = [
+          'type'=> 'template',
+          'altText'=> 'this is a buttons template',
+          'template'=> [
+              'type'=> 'buttons',
+              //'thumbnailImageUrl'=> 'https://example.com/bot/images/image.jpg',
+              'title'=> "ช่วงระหว่างการตั้งครรภ์คุณออกกำลังกายในระดับไหน",
+              'text'=> "Please select",
+              'actions'=> [
+                  [
+                    'type'=> 'message',
+                    'label'=> 'เบา',
+                    'text'=> 'เบา'
+                  ],
+                  [
+                    'type'=> 'message',
+                    'label'=> 'ปานกลาง',
+                    'text'=> 'ปานกลาง'
+                  ],
+                  [
+                    'type'=> 'message',
+                    'label'=> 'หนัก',
+                    'text'=> 'หนัก'
+                  ]
+              ]
+          ]
+        ];
 
+$q = pg_exec($dbconn, "UPDATE users_register SET  history_food = '{$_msg}' WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
+      $replyToken = $event['replyToken'];
 
-
-
-
-
-
-
-
-
-//  $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0027','','1001','0',NOW(),NOW())") or die(pg_errormessage());
-
-
-// ########################################################################################################################################################
-
-//         $messages = [
-//           'type'=> 'template',
-//           'altText'=> 'this is a buttons template',
-//           'template'=> [
-//               'type'=> 'buttons',
-//               //'thumbnailImageUrl'=> 'https://example.com/bot/images/image.jpg',
-//               'title'=> "ช่วงระหว่างการตั้งครรภ์คุณออกกำลังกายในระดับไหน",
-//               'text'=> "Please select",
-//               'actions'=> [
-//                   [
-//                     'type'=> 'message',
-//                     'label'=> 'เบา',
-//                     'text'=> 'เบา'
-//                   ],
-//                   [
-//                     'type'=> 'message',
-//                     'label'=> 'ปานกลาง',
-//                     'text'=> 'ปานกลาง'
-//                   ],
-//                   [
-//                     'type'=> 'message',
-//                     'label'=> 'หนัก',
-//                     'text'=> 'หนัก'
-//                   ]
-//               ]
-//           ]
-//         ];
-
-// /*.........................................................................................................................................*/
+ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0027','','1001','0',NOW(),NOW())") or die(pg_errormessage());
 
 
 // ############################################################### Summary ####################################################################################
