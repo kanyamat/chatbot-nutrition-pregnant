39f9cb6dd2672dce9ab08704efcc7386befc5b96
kanyamat <kanyamat003@gmail.com>
2017-11-16 09:45:28 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -763,6 +763,40 @@ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextse
     $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0023',$_msg,'0025','0',NOW(),NOW())") or die(pg_errormessage());
 
 ########################################################################################################################################################
+}elseif (strpos($_msg) !== false && $seqcode == "0025" && $event['message']['text'] == "ไม่แพ้ยา")  {
+               $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
+                while ($row = pg_fetch_row($result)) {
+                  echo $answer = $row[0]; 
+                }  
+                 $u = pg_escape_string($_msg); 
+                 $replyToken = $event['replyToken'];
+
+                  $messages = [
+                      'type' => 'template',
+                      'altText' => 'this is a confirm template',
+                      'template' => [
+                          'type' => 'confirm',
+                          'text' =>'คุณมีประวัติการแพ้อาหารไหมคะ?' ,
+                          'actions' => [
+                              [
+                                  'type' => 'message',
+                                  'label' => 'มี',
+                                  'text' => 'แพ้อาหาร'
+                              ],
+                              [
+                                  'type' => 'message',
+                                  'label' => 'ไม่มี',
+                                  'text' => 'ไม่แพ้อาหาร'
+                              ],
+                          ]
+                      ]
+                  ];        
+
+
+$q = pg_exec($dbconn, "UPDATE users_register SET  history_medicine ='{$u}' WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
+//$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0026','{$u}','0027','0',NOW(),NOW())") or die(pg_errormessage());
+
+########################################################################################################################################################
 
 
  }elseif ($event['message']['text'] == "เลขประจำตัวผู้ป่วยของถูกต้อง") {
@@ -1070,39 +1104,7 @@ $q = pg_exec($dbconn, "UPDATE users_register SET hospital_number = $answer WHERE
 
 
 
-########################################################################################################################################################
-}elseif (strpos($_msg) !== false && $seqcode == "0025" && $event['message']['text'] == "ไม่แพ้ยา")  {
-               $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
-                while ($row = pg_fetch_row($result)) {
-                  echo $answer = $row[0]; 
-                }  
-                 $u = pg_escape_string($_msg); 
-                 $replyToken = $event['replyToken'];
 
-                  $messages = [
-                      'type' => 'template',
-                      'altText' => 'this is a confirm template',
-                      'template' => [
-                          'type' => 'confirm',
-                          'text' =>'คุณมีประวัติการแพ้อาหารไหมคะ?' ,
-                          'actions' => [
-                              [
-                                  'type' => 'message',
-                                  'label' => 'มี',
-                                  'text' => 'แพ้อาหาร'
-                              ],
-                              [
-                                  'type' => 'message',
-                                  'label' => 'ไม่มี',
-                                  'text' => 'ไม่แพ้อาหาร'
-                              ],
-                          ]
-                      ]
-                  ];        
-
-
-$q = pg_exec($dbconn, "UPDATE users_register SET  history_medicine ='{$u}' WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
-//$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0026','{$u}','0027','0',NOW(),NOW())") or die(pg_errormessage());
 
 
 ############################################################### Summary ####################################################################################
