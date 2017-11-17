402020119b04fef8e505207b222aa91c458a2027
kanyamat <kanyamat003@gmail.com>
2017-11-17 11:35:44 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -987,64 +987,64 @@ $q = pg_exec($dbconn, "UPDATE users_register SET hospital_number = $answer WHERE
 
                  //  ];     
 
-                                  $messages = [
+                                  // $messages = [
+                                  //       'type'=> 'image',
+                                  //       'originalContentUrl'=> 'https://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat1.jpg',
+                                  //       'previewImageUrl'=> 'https://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat1.jpg'
+                                  //   ];
+                 $messages = [
+                      'type'=> 'template',
+                      'altText'=> 'this is a carousel template',
+                      'template'=> [
+                          'type'=> 'carousel',
+                          'columns'=> [
+                              [
+                                'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
+                                'title'=> 'this is menu',
+                                'text'=> 'description',
+                                'actions'=> [
+                                    // [
+                                    //     'type'=> 'image',
+                                    //     'originalContentUrl'=> 'Buy',
+                                    //     'previewImageUrl'=> 'https://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat1.jpg'
+                                    // ],
+                                    [
                                         'type'=> 'image',
                                         'originalContentUrl'=> 'https://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat1.jpg',
                                         'previewImageUrl'=> 'https://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat1.jpg'
-                                    ];
-                 // $messages = [
-                 //      'type'=> 'template',
-                 //      'altText'=> 'this is a carousel template',
-                 //      'template'=> [
-                 //          'type'=> 'carousel',
-                 //          'columns'=> [
-                 //              [
-                 //                'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
-                 //                'title'=> 'this is menu',
-                 //                'text'=> 'description',
-                 //                'actions'=> [
-                 //                    // [
-                 //                    //     'type'=> 'image',
-                 //                    //     'originalContentUrl'=> 'Buy',
-                 //                    //     'previewImageUrl'=> 'https://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat1.jpg'
-                 //                    // ],
-                 //                    [
-                 //                        'type'=> 'postback',
-                 //                        'label'=> 'Add to cart',
-                 //                        'data'=> 'action=add&itemid=111'
-                 //                    ],
-                 //                    [
-                 //                        'type'=> 'uri',
-                 //                        'label'=> 'View detail',
-                 //                        'uri'=> 'http://example.com/page/111'
-                 //                    ]
-                 //                ]
-                 //              ],
-                 //              [
-                 //                'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
-                 //                'title'=> 'this is menu',
-                 //                'text'=> 'description',
-                 //                'actions'=> [
-                 //                    // [
-                 //                    //     'type'=> 'postback',
-                 //                    //     'label'=> 'Buy',
-                 //                    //     'data'=> 'action=buy&itemid=111'
-                 //                    // ],
-                 //                    [
-                 //                        'type'=> 'postback',
-                 //                        'label'=> 'Add to cart',
-                 //                        'data'=> 'action=add&itemid=111'
-                 //                    ],
-                 //                    [
-                 //                        'type'=> 'uri',
-                 //                        'label'=> 'View detail',
-                 //                        'uri'=> 'http://example.com/page/111'
-                 //                    ]
-                 //                ]
-                 //              ]
-                 //          ]
-                 //      ]
-                 //    ];       
+                                    ],
+                                    [
+                                        'type'=> 'image',
+                                        'originalContentUrl'=> 'https://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat2.jpg',
+                                        'previewImageUrl'=> 'https://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat2.jpg'
+                                    ]
+                                ]
+                              ],
+                              [
+                                'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
+                                'title'=> 'this is menu',
+                                'text'=> 'description',
+                                'actions'=> [
+                                    // [
+                                    //     'type'=> 'postback',
+                                    //     'label'=> 'Buy',
+                                    //     'data'=> 'action=buy&itemid=111'
+                                    // ],
+                                    [
+                                        'type'=> 'postback',
+                                        'label'=> 'Add to cart',
+                                        'data'=> 'action=add&itemid=111'
+                                    ],
+                                    [
+                                        'type'=> 'uri',
+                                        'label'=> 'View detail',
+                                        'uri'=> 'http://example.com/page/111'
+                                    ]
+                                ]
+                              ]
+                          ]
+                      ]
+                    ];       
 ########################################################################################################################################################
 }elseif ($event['message']['text'] == "หนัก" || $event['message']['text'] == "ปานกลาง" || $event['message']['text'] == "เบา"  ) {
                  
