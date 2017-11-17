8a178b92255dfa5b99aa2f140ef3d9879ade75eb
kanyamat <kanyamat003@gmail.com>
2017-11-17 09:46:11 +0700

    d

--- chatbot.php
+++ chatbot.php
@@ -1127,9 +1127,12 @@ $q = pg_exec($dbconn, "UPDATE users_register SET hospital_number = $answer WHERE
                                     'uri' => ' http://www.raipoong.com/content/detail.php?section=12&category=26&id=467'
                                   ],
                                   [
-                                    'type' => 'message',
-                                    'label' => 'รูปภาพ',
-                                    'text' => 'รูปภาพ'
+                                    // 'type' => 'message',
+                                    // 'label' => 'รูปภาพ',
+                                    // 'text' => 'รูปภาพ'
+                                  'type'=> 'postback',
+                                  'label'=> 'รูปภาพ',
+                                  'data'=> 'รูปภาพ'
                                   ]
                                     
                                     
