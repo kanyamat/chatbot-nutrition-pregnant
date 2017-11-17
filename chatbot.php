259485767586ce2a302a52df48eca2fb52f1f5df
kanyamat <kanyamat003@gmail.com>
2017-11-17 11:14:51 +0700

    f

--- chatbot.php
+++ chatbot.php
@@ -952,40 +952,93 @@ $q = pg_exec($dbconn, "UPDATE users_register SET hospital_number = $answer WHERE
  }elseif ($event['message']['text'] == "รูปภาพ" ) {
                
                  $replyToken = $event['replyToken'];
-                 $messages = [
-                    'type'=> 'template',
-                    'altText'=> 'this is a image carousel template',
-                    'template'=> [
-                        'type'=> 'image_carousel',
-                        'columns'=> [
-                            [
-                              'imageUrl'=> 'http://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat1.jpg',
-                              'action'=> [
-                                'type'=> 'postback',
-                                'label'=> 'Buy',
-                                'data'=> 'action=buy&itemid=111'
-                              ]
-                            ],
-                            [
-                              'imageUrl'=> 'hthttp://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat2.jpg',
-                              'action'=> [
-                                'type'=> 'message',
-                                'label'=> 'Yes',
-                                'text'=> 'yes'
-                              ]
-                            ],
-                            [
-                              'imageUrl'=> 'http://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat2.jpg',
-                             'action'=> [
-                                'type'=> 'uri',
-                                'label'=> 'View detail',
-                                'uri'=> 'http://example.com/page/222'
-                              ]
-                            ]
-                        ]
-                    ]
+                 // $messages = [
+                 //    'type'=> 'template',
+                 //    'altText'=> 'this is a image carousel template',
+                 //    'template'=> [
+                 //        'type'=> 'image_carousel',
+                 //        'columns'=> [
+                 //            [
+                 //              'imageUrl'=> 'http://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat1.jpg',
+                 //              'action'=> [
+                 //                'type'=> 'postback',
+                 //                'label'=> 'Buy',
+                 //                'data'=> 'action=buy&itemid=111'
+                 //              ]
+                 //            ],
+                 //            [
+                 //              'imageUrl'=> 'hthttp://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat2.jpg',
+                 //              'action'=> [
+                 //                'type'=> 'message',
+                 //                'label'=> 'Yes',
+                 //                'text'=> 'yes'
+                 //              ]
+                 //            ],
+                 //            [
+                 //              'imageUrl'=> 'http://chatbot-nutrition-pregnant.herokuapp.com/Manual/eat2.jpg',
+                 //             'action'=> [
+                 //                'type'=> 'uri',
+                 //                'label'=> 'View detail',
+                 //                'uri'=> 'http://example.com/page/222'
+                 //              ]
+                 //            ]
+                 //        ]
+                 //    ]
 
-                  ];             
+                 //  ];     
+                 $messages = [
+                      'type'=> 'template',
+                      'altText'=> 'this is a carousel template',
+                      'template'=> [
+                          'type'=> 'carousel',
+                          'columns'=> [
+                              [
+                                'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
+                                'title'=> 'this is menu',
+                                'text'=> 'description',
+                                'actions'=> [
+                                    [
+                                        'type'=> 'postback',
+                                        'label'=> 'Buy',
+                                        'data'=> 'action=buy&itemid=111'
+                                    ],
+                                    [
+                                        'type'=> 'postback',
+                                        'label'=> 'Add to cart',
+                                        'data'=> 'action=add&itemid=111'
+                                    ],
+                                    [
+                                        'type'=> 'uri',
+                                        'label'=> 'View detail',
+                                        'uri'=> 'http://example.com/page/111'
+                                    ]
+                                ]
+                              ],
+                              [
+                                'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
+                                'title'=> 'this is menu',
+                                'text'=> 'description',
+                                'actions'=> [
+                                    [
+                                        'type'=> 'postback',
+                                        'label'=> 'Buy',
+                                        'data'=> 'action=buy&itemid=111'
+                                    ],
+                                    [
+                                        'type'=> 'postback',
+                                        'label'=> 'Add to cart',
+                                        'data'=> 'action=add&itemid=111'
+                                    ],
+                                    [
+                                        'type'=> 'uri',
+                                        'label'=> 'View detail',
+                                        'uri'=> 'http://example.com/page/111'
+                                    ]
+                                ]
+                              ]
+                          ]
+                      }
+                    ]       
 ########################################################################################################################################################
 }elseif ($event['message']['text'] == "หนัก" || $event['message']['text'] == "ปานกลาง" || $event['message']['text'] == "เบา"  ) {
                  
