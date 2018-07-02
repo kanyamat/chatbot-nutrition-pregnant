0225a1b0c1de729d66411c428f6a9fca67061594
kanyamat <31055915+kanyamat@users.noreply.github.com>
2018-07-02 17:49:50 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -1852,7 +1852,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
-$obj = json_encode($result);
+//$obj = json_encode($result);
+$json = json_encode($result);
 //$result2 = $result['description'];
 
 
