30bfe8596a797cadf091afdf83576fa6162d0d82
kanyamat <31055915+kanyamat@users.noreply.github.com>
2018-07-02 17:37:45 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -1852,13 +1852,13 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
-$result = $result['description'];
+$result2 = $result['description'];
 
 
                  $replyToken = $event['replyToken'];
                  $messages = [
                         'type' => 'text',
-                        'text' => $result
+                        'text' => $result2
                       ];    
 
 ########################################################################################################################################################
